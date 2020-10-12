a0d125f32c4486b192ae440a6bd3b80996976a1f
Alex Chi <alexchi@c4g.io>
2020-10-12 20:22:29 +0800

    update the doc for readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,10 @@ author = 'Alex Chi'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    'breathe',
+    'sphinx.ext.mathjax'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 #templates_path = ['_templates']
