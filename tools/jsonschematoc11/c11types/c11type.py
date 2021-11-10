298a54ca683affc7daad5d2f16622dca6fda15a5
Xing Ji <jixingcn@gmail.com>
2021-11-11 00:08:19 +0800

    fix some warnings in jsonschematoc11 again

--- tools/jsonschematoc11/c11types/c11type.py
+++ tools/jsonschematoc11/c11types/c11type.py
@@ -1,3 +1,5 @@
+import abc
+
 class C11Type(object):
 
     """basic type."""
@@ -26,35 +28,46 @@ class C11Type(object):
         if u'description' in schemaValue:
             self.description = schemaValue[u'description']
 
+    @abc.abstractmethod
     def revise(self, c11Types):
         return (0, u'')
 
+    @abc.abstractmethod
     def codeTypeName(self, withDeclare=False, asVariable=False, withDocument=False):
         return self.typeName
 
+    @abc.abstractmethod
     def codeDefineVariable(self, variableName, withDeclare=False, withDefault=False):
         pass
 
+    @abc.abstractmethod
     def codeSetDefaultInConstructor(self):
         pass
 
+    @abc.abstractmethod
     def codeHeader(self, codeTypeNames):
         return [u'']
 
+    @abc.abstractmethod
     def codeSource(self, codeTypeNames):
         return [u'']
 
+    @abc.abstractmethod
     def codeDefaultValue(self, schemaDefaultValue):
         return u''
 
+    @abc.abstractmethod
     def codeDefaultValueArray(self, schemaDefaultValue):
         return u''
 
+    @abc.abstractmethod
     def codeJsonCheck(self):
         return None
 
+    @abc.abstractmethod
     def codeJsonSet(self, dataName, variableName):
         return None
 
+    @abc.abstractmethod
     def codeJsonGet(self, dataName, variableName):
         return None
